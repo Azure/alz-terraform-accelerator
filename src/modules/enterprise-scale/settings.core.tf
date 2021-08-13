335cdb6a17f20ef2c86bb5c9688d5e9a7478892d
Kevin Rowlandson <kevin.rowlandson@microsoft.com>
2021-08-13 15:03:35 +0100

    Add archetype_config_overrides input with defaults

--- src/modules/enterprise-scale/settings.core.tf
+++ src/modules/enterprise-scale/settings.core.tf
@@ -32,6 +32,84 @@ locals {
   }
 }
 
+# Configure the archetype config overrides to customize
+# the configuration. This repository comes pre-populated
+# with default values, as specified in the module.
+# Uncomment any you want to update.
+locals {
+  archetype_config_overrides = {
+    # root = {
+    #   archetype_id   = "es_root"
+    #   parameters     = {}
+    #   access_control = {}
+    # }
+    # decommissioned = {
+    #   archetype_id   = "es_decommissioned"
+    #   parameters     = {}
+    #   access_control = {}
+    # }
+    # sandboxes = {
+    #   archetype_id   = "es_sandboxes"
+    #   parameters     = {}
+    #   access_control = {}
+    # }
+    # landing-zones = {
+    #   archetype_id   = "es_landing_zones"
+    #   parameters     = {}
+    #   access_control = {}
+    # }
+    # platform = {
+    #   archetype_id   = "es_platform"
+    #   parameters     = {}
+    #   access_control = {}
+    # }
+    # connectivity = {
+    #   archetype_id   = "es_connectivity"
+    #   parameters     = {}
+    #   access_control = {}
+    # }
+    # management = {
+    #   archetype_id   = "es_management"
+    #   parameters     = {}
+    #   access_control = {}
+    # }
+    # identity = {
+    #   archetype_id   = "es_identity"
+    #   parameters     = {}
+    #   access_control = {}
+    # }
+    # corp = {
+    #   archetype_id   = "es_corp"
+    #   parameters     = {}
+    #   access_control = {}
+    # }
+    # online = {
+    #   archetype_id   = "es_online"
+    #   parameters     = {}
+    #   access_control = {}
+    # }
+    # sap = {
+    #   archetype_id   = "es_sap"
+    #   parameters     = {}
+    #   access_control = {}
+    # }
+    # demo-corp = {
+    #   archetype_id   = "es_corp"
+    #   parameters     = {}
+    #   access_control = {}
+    # }
+    # demo-online = {
+    #   archetype_id   = "es_online"
+    #   parameters     = {}
+    #   access_control = {}
+    # }
+    # demo-sap = {
+    #   archetype_id   = "es_sap"
+    #   parameters     = {}
+    #   access_control = {}
+    # }
+  }
+}
 
 # Configure the Subscription ID overrides to ensure
 # Subscriptions are moved into the target management
@@ -46,6 +124,9 @@ locals {
     connectivity   = [] # DO NOT USE - Use `subscription_id_connectivity` input variable on module instead
     management     = [] # DO NOT USE - Use `subscription_id_management` input variable on module instead
     identity       = [] # DO NOT USE - Use `subscription_id_identity` input variable on module instead
+    corp           = []
+    online         = []
+    sap            = []
     demo-corp      = []
     demo-online    = []
     demo-sap       = []
