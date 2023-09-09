5b75d8db7d0657bc6d9a5ee1a39468a681f5015a
Jared Holgate <jaredholgate@microsoft.com>
2023-09-09 11:48:17 +0100

    Fix typo

--- bootstrap/modules/azure_devops/environment.tf
+++ bootstrap/modules/azure_devops/environment.tf
@@ -14,7 +14,7 @@ resource "azuredevops_check_approval" "alz" {
   target_resource_id   = azuredevops_environment.alz_apply.id
   target_resource_type = "environment"
 
-  requester_can_approve = length(var.appovers) == 1
+  requester_can_approve = length(var.approvers) == 1
   approvers = [
     azuredevops_group.alz_approvers.origin_id
   ]
