b6e818b7aad77073f5cd50cd420f9254c96d750c
Jared Holgate <jaredholgate@microsoft.com>
2023-09-11 21:56:16 +0100

    Update number validation

--- templates/.test/variables.tf
+++ templates/.test/variables.tf
@@ -5,26 +5,26 @@ variable "test_variable_01" {
 
 variable "test_variable_02" {
   type = number
-  description = "This is the second test variable"
+  description = "This is the second test variable|number"
 }
 
 variable "test_variable_03" {
   type = bool
-  description = "This is the second test variable|bool"
+  description = "This is the third test variable|bool"
 }
 
 variable "test_variable_04" {
   type = string
-  description = "This is the second test variable|azure_location"
+  description = "This is the fourth test variable|azure_location"
 }
 
 variable "test_variable_05" {
   type = string
-  description = "This is the second test variable|guid"
+  description = "This is the fifth test variable|guid"
 }
 
 variable "test_variable_06" {
   type = string
-  description = "Default|azure_name"
+  description = "This is the sixth test variable.|azure_name"
   default = "testing-123"
 }
