6d368e42e7da9c632a0f60879f3d59ded4429113
Jared Holgate <jaredholgate@microsoft.com>
2023-09-14 10:16:44 +0100

    Add ordering capability

--- bootstrap/azuredevops/variables.tf
+++ bootstrap/azuredevops/variables.tf
@@ -1,63 +1,63 @@
 variable "starter_module" {
-  description = "The starter module to use for the deployment. (e.g. 'basic')|starter_module"
+  description = "The starter module to use for the deployment. (e.g. 'basic')|1|starter_module"
   type        = string
   default     = "basic"
 }
 
 variable "version_control_system_access_token" {
-  description = "The personal access token for the version control system to use for the deployment|azure_name"
+  description = "The personal access token for the version control system to use for the deployment|2|azure_name"
   type        = string
   sensitive   = true
 }
 
 variable "version_control_system_organization" {
-  description = "The organization for the version control system to use for the deployment|azure_name"
+  description = "The organization for the version control system to use for the deployment|3|azure_name"
   type        = string
 }
 
 variable "azure_location" {
-  description = "Azure Deployment location for the landing zone management resources|azure_location"
+  description = "Azure Deployment location for the landing zone management resources|4|azure_location"
   type        = string
 }
 
 variable "service_name" {
-  description = "Used to build up the default resource names (e.g. rg-<service_name>-mgmt-uksouth-001)|azure_name_section"
+  description = "Used to build up the default resource names (e.g. rg-<service_name>-mgmt-uksouth-001)|5|azure_name_section"
   type        = string
   default     = "alz"
 }
 
 variable "environment_name" {
-  description = "Used to build up the default resource names (e.g. rg-alz-<environment_name>-uksouth-001)|azure_name_section"
+  description = "Used to build up the default resource names (e.g. rg-alz-<environment_name>-uksouth-001)|6|azure_name_section"
   type        = string
   default     = "mgmt"
 }
 
 variable "postfix_number" {
-  description = "Used to build up the default resource names (e.g. rg-alz-mgmt-uksouth-<postfix_number>)|number"
+  description = "Used to build up the default resource names (e.g. rg-alz-mgmt-uksouth-<postfix_number>)|7|number"
   type        = number
   default     = 1
 }
 
 variable "azure_devops_use_organisation_legacy_url" {
-  description = "Use the legacy Azure DevOps URL (<organisation>.visualstudio.com) instead of the new URL (dev.azure.com/<organization>)|bool"
+  description = "Use the legacy Azure DevOps URL (<organisation>.visualstudio.com) instead of the new URL (dev.azure.com/<organization>)|8|bool"
   type        = bool
   default     = false
 }
 
 variable "azure_devops_create_project" {
-  description = "Create the Azure DevOps project if it does not exist|bool"
+  description = "Create the Azure DevOps project if it does not exist|9|bool"
   type        = bool
   default     = true
 }
 
 variable "azure_devops_project_name" {
-  description = "The name of the Azure DevOps project to use or create for the deployment|azure_name"
+  description = "The name of the Azure DevOps project to use or create for the deployment|10|azure_name"
   type        = string
 }
 
 variable "azure_devops_authentication_scheme" {
   type        = string
-  description = "The authentication scheme to use for the Azure DevOps Pipelines|auth_scheme"
+  description = "The authentication scheme to use for the Azure DevOps Pipelines|11|auth_scheme"
   validation {
     condition     = can(regex("^(ManagedServiceIdentity|WorkloadIdentityFederation)$", var.azure_devops_authentication_scheme))
     error_message = "azure_devops_authentication_scheme must be either ManagedServiceIdentity or WorkloadIdentityFederation"
@@ -66,7 +66,7 @@ variable "azure_devops_authentication_scheme" {
 }
 
 variable "apply_approvers" {
-  description = "Apply stage approvers to the action / pipeline, must be a list of SPNs separate by a comma (e.g. abcdef@microsoft.com,ghijklm@microsoft.com)"
+  description = "Apply stage approvers to the action / pipeline, must be a list of SPNs separate by a comma (e.g. abcdef@microsoft.com,ghijklm@microsoft.com)|12"
   type        = list(string)
   default     = []
 }
--- templates/basic/variables.tf
+++ templates/basic/variables.tf
@@ -1,31 +1,31 @@
 variable "default_location" {
-  description = "The location for Azure resources. (e.g 'uksouth')|azure_location"
+  description = "The location for Azure resources. (e.g 'uksouth')|1|azure_location"
   type        = string
 }
 
 variable "subscription_id_connectivity" {
-  description = "The identifier of the Connectivity Subscription. (e.g '00000000-0000-0000-0000-000000000000')|guid"
+  description = "The identifier of the Connectivity Subscription. (e.g '00000000-0000-0000-0000-000000000000')|2|guid"
   type        = string
 }
 
 variable "subscription_id_identity" {
-  description = "The identifier of the Identity Subscription. (e.g '00000000-0000-0000-0000-000000000000')|guid"
+  description = "The identifier of the Identity Subscription. (e.g '00000000-0000-0000-0000-000000000000')|3|guid"
   type        = string
 }
 
 variable "subscription_id_management" {
-  description = "The identifier of the Management Subscription. (e.g 00000000-0000-0000-0000-000000000000)|guid"
+  description = "The identifier of the Management Subscription. (e.g 00000000-0000-0000-0000-000000000000)|4|guid"
   type        = string
 }
 
 variable "root_id" {
-  description = "value of the root id|azure_name"
+  description = "value of the root id|5|azure_name"
   type        = string
   default     = "es"
 }
 
 variable "root_name" {
-  description = "value of the root name|azure_name"
+  description = "value of the root name|6|azure_name"
   type        = string
   default     = "Enterprise-Scale"
 }
