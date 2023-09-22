8f65c6662abd2daedff0dd5146aecdbceaee27c2
luke-taylor <lukejamestaylor98@outlook.com>
2023-09-22 13:34:58 +0100

    add complete

---
+++ templates/complete/variables.tf
@@ -0,0 +1,14 @@
+variable "subscription_id_connectivity" {
+  description = "value of the subscription id for the Connectivity subscription"
+  type        = string
+}
+
+variable "subscription_id_identity" {
+  description = "value of the subscription id for the Identity subscription"
+  type        = string
+}
+
+variable "subscription_id_management" {
+  description = "value of the subscription id for the Management subscription"
+  type        = string
+}
