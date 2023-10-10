8f68653b5a78a98ce03344ca2745da30a5f1f480
Jared Holgate <jaredholgate@microsoft.com>
2023-10-10 17:16:25 +0100

    Increase retries for getting the version

--- tests/scripts/get-release.ps1
+++ tests/scripts/get-release.ps1
@@ -1,5 +1,5 @@
 param (
-    [int]$maximumRetries = 10,
+    [int]$maximumRetries = 50,
     [int]$retryCount = 0,
     [int]$retryDelay = 10000
 )
@@ -15,7 +15,7 @@ do {
         $release = $($releaseObject.name)
         $success = $true
     } catch {
-        Write-Host "Failed to get the release version."
+        Write-Host "Failed to get the release version. Retrying after $retryDelay ms..."
         Start-Sleep -Milliseconds $retryDelay
     }
 } while ($success -eq $false -and $retryCount -lt $maximumRetries)
