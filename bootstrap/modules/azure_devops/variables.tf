001f70f0d86ad0d0bdc6b1423066318b4612ca34
Jared Holgate <jaredholgate@microsoft.com>
2023-12-02 19:10:10 +0000

    Tidy description

--- bootstrap/modules/azure_devops/variables.tf
+++ bootstrap/modules/azure_devops/variables.tf
@@ -1,5 +1,5 @@
 variable "version_control_system_access_token" {
-  description = "The personal access token for the version control system to use for the deployment|2"
+  description = "The personal access token for the version control system to use for the extension install"
   type        = string
   sensitive   = true
 }
