ece7715d3ff3d6b926c8bf4922d588618ed83fa9
Jared Holgate <jaredholgate@microsoft.com>
2023-12-11 14:37:45 +0000

    feature: complete modular starter module (#30)
    <!-- Thank you for submitting a Pull Request. Please fill out the
    template below.-->
    ## Overview/Summary
    
    The PR adds an end to end example of using the new avm modules for ALZ.
    
    ## This PR fixes/adds/changes/removes
    
    1. #29
    
    ### Breaking Changes
    
    None
    
    ## Testing Evidence
    
    The starter module has been run and generated a hierarchy with no
    errors:
    
    ![image](https://github.com/Azure/alz-terraform-accelerator/assets/1612200/12dd11a6-cef6-4791-836a-2e6838571b40)
    
    ## As part of this Pull Request I have
    
    - [x] Checked for duplicate [Pull
    Requests](https://github.com/Azure/alz-terraform-accelerator/pulls)
    - [x] Associated it with relevant
    [issues](https://github.com/Azure/alz-terraform-accelerator/issues), for
    tracking and closure.
    - [x] Ensured my code/branch is up-to-date with the latest changes in
    the `main`
    [branch](https://github.com/Azure/alz-terraform-accelerator/tree/main)
    - [x] Performed testing and provided evidence.
    - [x] Updated relevant and associated documentation.

---
+++ templates/complete_vnext/variables.tf
@@ -0,0 +1,31 @@
+variable "default_location" {
+  description = "The location for Azure resources. (e.g 'uksouth')|1|azure_location"
+  type        = string
+}
+
+variable "default_postfix" {
+  description = "The default postfix for Azure resources. (e.g 'landing-zone')|2|azure_name"
+  type        = string
+  default     = "landing-zone"
+}
+
+variable "root_management_group_id" {
+  description = "The identifier of the Tenant Root Management Group, if left blank will use the tenant id. (e.g '00000000-0000-0000-0000-000000000000')|3|azure_name"
+  type        = string
+  default     = ""
+}
+
+variable "subscription_id_connectivity" {
+  description = "The identifier of the Connectivity Subscription. (e.g '00000000-0000-0000-0000-000000000000')|4|azure_subscription_id"
+  type        = string
+}
+
+variable "subscription_id_identity" {
+  description = "The identifier of the Identity Subscription. (e.g '00000000-0000-0000-0000-000000000000')|5|azure_subscription_id"
+  type        = string
+}
+
+variable "subscription_id_management" {
+  description = "The identifier of the Management Subscription. (e.g 00000000-0000-0000-0000-000000000000)|6|azure_subscription_id"
+  type        = string
+}
