cd015d0593c7e613875c1b28cfd0cb38b4d8288b
Jared Holgate <jaredholgate@microsoft.com>
2024-01-10 11:50:08 +0000

    improve output value

--- bootstrap/local/outputs.tf
+++ bootstrap/local/outputs.tf
@@ -1,4 +1,4 @@
 output "module_output_directory_path" {
   description = "The path to the directory where the module files have been created."
-  value       = local.target_directory
+  value       = abspath(local.target_directory)
 }
