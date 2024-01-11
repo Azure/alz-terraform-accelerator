063699b19db60961a2aa0fe57a53be5ca7a0ea11
Jared Holgate <jaredholgate@microsoft.com>
2024-01-11 22:51:52 +0000

    Slim down tests

--- tests/cleanup_resouce_groups.ps1
+++ tests/cleanup_resouce_groups.ps1
@@ -1,9 +1,25 @@
 # This file can be used to clean up Resource Groups if there has been an issue with the End to End tests.
 # CAUTION: Make sure you are connected to the correct subscription before running this script!
 az account show
-$resourceGroups = az group list --query "[?contains(name, '223-')]" | ConvertFrom-Json
+$resourceGroups = az group list --query "[?contains(name, '225-')]" | ConvertFrom-Json
 
 $resourceGroups | ForEach-Object -Parallel {
     Write-Host "Deleting resource group: $($_.name)"
     az group delete --name $_.name --yes
+} -ThrottleLimit 10
+
+
+# GitHub
+$repos = gh repo list microsoft-azure-landing-zones-cd-tests --json name,owner | ConvertFrom-Json
+
+$repos | ForEach-Object -Parallel {
+    $match = "*168*"
+    $repoName = "$($_.owner.login)/$($_.name)"
+    
+    if($repoName -like $match)
+    {
+        Write-Host "Deleting repo: $repoName"
+        gh repo delete $repoName --yes
+    
+    }
 } -ThrottleLimit 10
