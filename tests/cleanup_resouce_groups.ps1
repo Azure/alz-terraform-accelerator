50e3894198861f083c2bff015bc4c4c0d26b8857
Jared Holgate <jaredholgate@microsoft.com>
2024-01-11 22:53:41 +0000

    bosh the throttle

--- tests/cleanup_resouce_groups.ps1
+++ tests/cleanup_resouce_groups.ps1
@@ -1,7 +1,7 @@
 # This file can be used to clean up Resource Groups if there has been an issue with the End to End tests.
 # CAUTION: Make sure you are connected to the correct subscription before running this script!
 az account show
-$resourceGroups = az group list --query "[?contains(name, '225-')]" | ConvertFrom-Json
+$resourceGroups = az group list --query "[?contains(name, '227-')]" | ConvertFrom-Json
 
 $resourceGroups | ForEach-Object -Parallel {
     Write-Host "Deleting resource group: $($_.name)"
