a0b901cec6a284ab5aa673f5c41ef36356945216
Jared Holgate <jaredholgate@microsoft.com>
2024-11-01 18:46:04 +0000

    feat: add avm management group support

--- templates/complete_multi_region/terraform.tf
+++ templates/complete_multi_region/terraform.tf
@@ -1,6 +1,10 @@
 terraform {
   required_version = "~> 1.9"
   required_providers {
+    alz = {
+      source  = "Azure/alz"
+      version = "~> 0.16"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.107"
@@ -15,6 +19,13 @@ terraform {
     }
   }
   # backend "azurerm" {}
+}
+
+provider "alz" {
+  library_references = [{
+    path = "platform/alz"
+    ref  = "2024.10.1"
+  }]
 }
 
 provider "azapi" {
