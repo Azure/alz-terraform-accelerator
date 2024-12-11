aaba94cbf8787d8962b995a5d70850f5676b5f15
Jared Holgate <jaredholgate@microsoft.com>
2024-12-11 20:10:20 +0000

    Fix outputs and tidy code

--- templates/platform_landing_zone/outputs.tf
+++ templates/platform_landing_zone/outputs.tf
@@ -1,3 +1,3 @@
 output "dns_server_ip_addresses" {
-  value = var.connectivity_type == "none" ? {} : (var.connectivity_type == "hub-and-spoke" ? module.hub_and_spoke_vnet.dns_server_ip_addresses : module.virtual_wan.dns_server_ip_addresses)
+  value = local.connectivity_enabled ? (local.connectivity_hub_and_spoke_vnet_enabled ? module.hub_and_spoke_vnet[0].dns_server_ip_addresses : module.virtual_wan[0].dns_server_ip_addresses) : {}
 }
