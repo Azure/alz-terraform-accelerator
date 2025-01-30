a0877baede2d8a19bb2a8d876f36b8b625b2b2f5
Jared Holgate <jaredholgate@microsoft.com>
2025-01-30 15:33:12 +0000

    chore: remove deprecated starter modules

--- templates/complete_multi_region/locals-resource-groups.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  connectivity_resource_groups_json           = tostring(var.skip_deploy ? jsonencode({}) : jsonencode(var.connectivity_resource_groups))
-  connectivity_resource_groups_json_templated = templatestring(local.connectivity_resource_groups_json, local.config_template_file_variables)
-  connectivity_resource_groups_json_final     = replace(replace(local.connectivity_resource_groups_json_templated, "\"[", "["), "]\"", "]")
-  connectivity_resource_groups                = jsondecode(local.connectivity_resource_groups_json_final)
-}
