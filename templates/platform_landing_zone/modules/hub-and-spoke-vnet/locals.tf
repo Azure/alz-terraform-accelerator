a4a5722432716c41ea20b70becdc5bd6448a9677
Jared Holgate <jaredholgate@microsoft.com>
2025-01-30 19:50:42 +0000

    fix: remove unrequired config settings (#212)

--- templates/platform_landing_zone/modules/hub-and-spoke-vnet/locals.tf
+++ templates/platform_landing_zone/modules/hub-and-spoke-vnet/locals.tf
@@ -1,7 +1,10 @@
 locals {
   hub_virtual_networks = {
     for key, value in var.hub_virtual_networks : key => merge(value.hub_virtual_network, {
-      ddos_protection_plan_id = local.ddos_protection_plan_enabled ? module.ddos_protection_plan[0].resource.id : try(value.hub_virtual_network.ddos_protection_plan_id, null)
+      ddos_protection_plan_id         = local.ddos_protection_plan_enabled ? module.ddos_protection_plan[0].resource.id : try(value.hub_virtual_network.ddos_protection_plan_id, null)
+      resource_group_creation_enabled = try(value.hub_virtual_network.resource_group_creation_enabled, false)
+      resource_group_lock_enabled     = try(value.hub_virtual_network.resource_group_lock_enabled, false)
+      mesh_peering_enabled            = try(value.hub_virtual_network.mesh_peering_enabled, true)
       firewall = try(value.hub_virtual_network.firewall, null) == null ? null : merge(value.hub_virtual_network.firewall, {
         firewall_policy = merge(value.hub_virtual_network.firewall.firewall_policy, {
           dns = merge({
