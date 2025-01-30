dfd85c066b00901e8177c18361326cf0c7b4b115
Jared Holgate <jaredholgate@microsoft.com>
2025-01-30 11:11:08 +0000

    fix: turn off private dns

--- templates/platform_landing_zone/modules/hub-and-spoke-vnet/locals.dns.tf
+++ templates/platform_landing_zone/modules/hub-and-spoke-vnet/locals.dns.tf
@@ -10,7 +10,7 @@ locals {
   private_dns_zones_auto_registration = { for key, value in var.hub_virtual_networks : key => merge({
     location         = value.hub_virtual_network.location
     vnet_resource_id = module.hub_and_spoke_vnet.virtual_networks[key].id
-  }, value.private_dns_zones) if local.private_dns_zones_enabled[key] && value.private_dns_zones.auto_registration_zone_enabled }
+  }, value.private_dns_zones) if local.private_dns_zones_enabled[key] && try(value.private_dns_zones.auto_registration_zone_enabled, false) }
 
   private_dns_zones_virtual_network_links = {
     for key, value in module.hub_and_spoke_vnet.virtual_networks : key => {
