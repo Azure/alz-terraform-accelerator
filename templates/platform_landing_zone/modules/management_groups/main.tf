437d0e44bd2eaadfb019e1b041e93739720c0f05
Jared Holgate <jaredholgate@microsoft.com>
2025-02-17 12:06:12 +0000

    feat: add policy assignment role assignment override variables

--- templates/platform_landing_zone/modules/management_groups/main.tf
+++ templates/platform_landing_zone/modules/management_groups/main.tf
@@ -1,17 +1,19 @@
 module "management_groups" {
-  source                              = "Azure/avm-ptn-alz/azurerm"
-  version                             = "0.11.0"
-  architecture_name                   = try(var.management_group_settings.architecture_name, "alz")
-  parent_resource_id                  = var.management_group_settings.parent_resource_id
-  location                            = var.management_group_settings.location
-  policy_default_values               = local.policy_default_values
-  policy_assignments_to_modify        = local.policy_assignments_to_modify
-  delays                              = try(var.management_group_settings.delays, {})
-  enable_telemetry                    = var.enable_telemetry
-  management_group_hierarchy_settings = try(var.management_group_settings.management_group_hierarchy_settings, null)
-  partner_id                          = try(var.management_group_settings.partner_id, null)
-  retries                             = try(var.management_group_settings.retries, local.default_retries)
-  subscription_placement              = try(var.management_group_settings.subscription_placement, {})
-  timeouts                            = try(var.management_group_settings.timeouts, local.default_timeouts)
-  dependencies                        = var.dependencies
+  source                                                        = "Azure/avm-ptn-alz/azurerm"
+  version                                                       = "0.11.0"
+  architecture_name                                             = try(var.management_group_settings.architecture_name, "alz")
+  parent_resource_id                                            = var.management_group_settings.parent_resource_id
+  location                                                      = var.management_group_settings.location
+  policy_default_values                                         = local.policy_default_values
+  policy_assignments_to_modify                                  = local.policy_assignments_to_modify
+  delays                                                        = try(var.management_group_settings.delays, {})
+  enable_telemetry                                              = var.enable_telemetry
+  management_group_hierarchy_settings                           = try(var.management_group_settings.management_group_hierarchy_settings, null)
+  partner_id                                                    = try(var.management_group_settings.partner_id, null)
+  retries                                                       = try(var.management_group_settings.retries, local.default_retries)
+  subscription_placement                                        = try(var.management_group_settings.subscription_placement, {})
+  timeouts                                                      = try(var.management_group_settings.timeouts, local.default_timeouts)
+  dependencies                                                  = var.dependencies
+  override_policy_definition_parameter_assign_permissions_set   = try(var.management_group_settings.override_policy_definition_parameter_assign_permissions_set, null)
+  override_policy_definition_parameter_assign_permissions_unset = try(var.management_group_settings.override_policy_definition_parameter_assign_permissions_unset, null)
 }
