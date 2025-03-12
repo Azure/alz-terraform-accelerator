2f884e162250436eaa30ae30d387c7824d318cf3
Jared Holgate <jaredholgate@microsoft.com>
2025-03-12 20:24:55 +0000

    feat: use pattern modules for connectivity (#222)
    <!-- Thank you for submitting a Pull Request. Please fill out the
    template below.-->
    ## Overview/Summary
    
    Replace the connectivity submodules with AVM pattern modules to simplify
    the user upgrade process.
    
    ## This PR fixes/adds/changes/removes
    
    N/A
    
    ### Breaking Changes
    
    None, state is the same.
    
    ## Testing Evidence
    
    Ran apply with main, then ran apply with this branch. All changes were
    for modtm only.
    
    ## As part of this Pull Request I have
    
    - [x] Checked for duplicate [Pull
    Requests](https://github.com/Azure/alz-terraform-accelerator/pulls)
    - [x] Associated it with relevant
    [issues](https://github.com/Azure/alz-terraform-accelerator/issues), for
    tracking and closure.
    - [x] Ensured my code/branch is up-to-date with the latest changes in
    the `main`
    [branch](https://github.com/Azure/alz-terraform-accelerator/tree/main)
    - [x] Performed testing and provided evidence.
    - [x] Updated relevant and associated documentation.

--- templates/platform_landing_zone/modules/virtual-wan/locals.ddos.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  ddos_protection_plan         = try(var.virtual_wan_settings.ddos_protection_plan.name, null) != null ? var.virtual_wan_settings.ddos_protection_plan : null
-  ddos_protection_plan_enabled = local.ddos_protection_plan != null
-}
--- templates/platform_landing_zone/modules/virtual-wan/locals.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  virtual_hubs = { for virtual_hub_key, virtual_hub_value in var.virtual_hubs : virtual_hub_key => virtual_hub_value.hub }
-}
