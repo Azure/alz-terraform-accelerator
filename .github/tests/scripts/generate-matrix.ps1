6792ed0d1b2c359fb27e876a72412c2efef927ba
Jared Holgate <jaredholgate@microsoft.com>
2025-06-10 17:03:46 +0100

    add combinations to end to end tests

--- .github/tests/scripts/generate-matrix.ps1
+++ .github/tests/scripts/generate-matrix.ps1
@@ -8,6 +8,12 @@ $configFiles = Get-ChildItem -Path $exampleFolders -Recurse -Filter "*.tfvars" -
 
 $matrix = @()
 
+function Recurse-Config {
+  param (
+    [array]$booleanConfigs
+  )
+}
+
 foreach($configFile in $configFiles) {
 
   $directory = $configFile.Directory.Name
@@ -16,11 +22,42 @@ foreach($configFile in $configFiles) {
   $shortDirectory = [System.String]::Join("", ($directory.Split("-") | ForEach-Object { $_.Substring(0, 1)}))
   $shortFileName = [System.String]::Join("", ($configFileName.Split("-") | ForEach-Object { $_.Substring(0, 1)}))
 
+  $fileContent = Get-Content -Path $configFile.FullName
+
+  Write-Host "Processing config file: $($configFile.FullName)"
+
+  $booleanConfigs = @()
+  foreach($line in $fileContent) {
+    if($line -match "^.+_enabled\s+=\strue$") {
+      Write-Host "Found boolean config: $($line)"
+      $booleanConfigs += $line
+    }
+  }
+
+  $everyCombination = @()
+  $configState = @{}
+
+  foreach($config in $booleanConfigs) {
+    $configState[$config] = $true
+  }
+
+  foreach($config in $booleanConfigs) {
+    foreach($childConfig in $booleanConfigs) {
+      $combination = $configState.Clone()
+      $everyCombination += $combination
+      $configState[$childConfig] = !$configState[$childConfig]
+    }
+    $configState[$config] = !$configState[$config]
+  }
+
+  Write-Host "Found $($everyCombination.Count) combinations for $($configFile.FullName)"
+
   $matrixItem = @{
     name = $directory + "-" + $configFileName + "-" + $runNumber
     shortName = $shortDirectory + $shortFileName + "-" + $runNumber
     configFilePath = $configFile.FullName
     rootModuleFolderPath = $rootModuleFolder
+    combinations = $everyCombination
   }
   $matrix += $matrixItem
 }
