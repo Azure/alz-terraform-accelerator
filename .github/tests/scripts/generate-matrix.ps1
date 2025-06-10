712b9ea1fe22b40d39f596ec2def06d693196027
Jared Holgate <jaredholgate@microsoft.com>
2025-06-10 17:09:21 +0100

    fix tests

--- .github/tests/scripts/generate-matrix.ps1
+++ .github/tests/scripts/generate-matrix.ps1
@@ -22,42 +22,11 @@ foreach($configFile in $configFiles) {
   $shortDirectory = [System.String]::Join("", ($directory.Split("-") | ForEach-Object { $_.Substring(0, 1)}))
   $shortFileName = [System.String]::Join("", ($configFileName.Split("-") | ForEach-Object { $_.Substring(0, 1)}))
 
-  $fileContent = Get-Content -Path $configFile.FullName
-
-  Write-Host "Processing config file: $($configFile.FullName)"
-
-  $booleanConfigs = @()
-  foreach($line in $fileContent) {
-    if($line -match "^.+_enabled\s+=\strue$") {
-      Write-Host "Found boolean config: $($line)"
-      $booleanConfigs += $line
-    }
-  }
-
-  $everyCombination = @()
-  $configState = @{}
-
-  foreach($config in $booleanConfigs) {
-    $configState[$config] = $true
-  }
-
-  foreach($config in $booleanConfigs) {
-    foreach($childConfig in $booleanConfigs) {
-      $combination = $configState.Clone()
-      $everyCombination += $combination
-      $configState[$childConfig] = !$configState[$childConfig]
-    }
-    $configState[$config] = !$configState[$config]
-  }
-
-  Write-Host "Found $($everyCombination.Count) combinations for $($configFile.FullName)"
-
   $matrixItem = @{
     name = $directory + "-" + $configFileName + "-" + $runNumber
     shortName = $shortDirectory + $shortFileName + "-" + $runNumber
     configFilePath = $configFile.FullName
     rootModuleFolderPath = $rootModuleFolder
-    combinations = $everyCombination
   }
   $matrix += $matrixItem
 }
