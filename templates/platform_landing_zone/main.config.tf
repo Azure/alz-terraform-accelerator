35a7cbd0a4910857ff9ae7003ea9777f8eaa49e6
Jared Holgate <jaredholgate@microsoft.com>
2025-09-16 11:25:34 +0100

    fix: backwards compat for security

--- templates/platform_landing_zone/main.config.tf
+++ templates/platform_landing_zone/main.config.tf
@@ -7,7 +7,7 @@ module "config" {
   subscription_id_connectivity    = try(var.subscription_ids["connectivity"], var.subscription_id_connectivity)
   subscription_id_identity        = try(var.subscription_ids["identity"], var.subscription_id_identity)
   subscription_id_management      = try(var.subscription_ids["management"], var.subscription_id_management)
-  subscription_id_security        = var.subscription_ids["security"]
+  subscription_id_security        = try(var.subscription_ids["security"], "")
   root_parent_management_group_id = var.root_parent_management_group_id
 
   custom_replacements = var.custom_replacements
