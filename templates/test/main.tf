81a264bc47fb34d96f7c65f13f09557f480f9292
Jared Holgate <jaredholgate@microsoft.com>
2026-01-15 13:26:42 +0000

    feat: improve tests

--- templates/test/main.tf
+++ templates/test/main.tf
@@ -9,47 +9,42 @@ locals {
   starter_location = var.starter_locations[0]
 }
 
-resource "random_string" "example" {
-  length  = 10
-  special = false
-  numeric = false
-  upper   = false
-}
-
-resource "azurerm_management_group" "example_child" {
-  name                       = "e2e-test-${random_string.example.result}"
-  display_name               = "${var.child_management_group_display_name} ${random_string.example.result}"
-  parent_management_group_id = data.azurerm_management_group.example_parent.id
+module "management_groups" {
+  source                                                        = "Azure/avm-ptn-alz/azurerm"
+  version                                                       = "0.14.1"
+  architecture_name                                             = "alz_custom"
+  parent_resource_id                                            = data.azurerm_management_group.example_parent.name
+  location                                                      = local.starter_location
 }
 
 resource "azurerm_resource_group" "management" {
   provider = azurerm.management
-  name     = "e2e-test-management-azurerm-${random_string.example.result}"
+  name     = "e2e-test-management-azurerm-${var.resource_name_suffix}"
   location = local.starter_location
 }
 
 resource "azurerm_resource_group" "connectivity" {
   provider = azurerm.connectivity
-  name     = "e2e-test-connectivity-azurerm-${random_string.example.result}"
+  name     = "e2e-test-connectivity-azurerm-${var.resource_name_suffix}"
   location = local.starter_location
 }
 
 resource "azurerm_resource_group" "identity" {
   provider = azurerm.identity
-  name     = "e2e-test-identity-azurerm-${random_string.example.result}"
+  name     = "e2e-test-identity-azurerm-${var.resource_name_suffix}"
   location = local.starter_location
 }
 
 resource "azurerm_resource_group" "security" {
   provider = azurerm.security
-  name     = "e2e-test-security-azurerm-${random_string.example.result}"
+  name     = "e2e-test-security-azurerm-${var.resource_name_suffix}"
   location = local.starter_location
 }
 
 resource "azapi_resource" "resource_group_management" {
   parent_id = "/subscriptions/${var.subscription_ids["management"]}"
   type      = "Microsoft.Resources/resourceGroups@2021-04-01"
-  name      = "e2e-test-management-azapi-${random_string.example.result}"
+  name      = "e2e-test-management-azapi-${var.resource_name_suffix}"
   location  = local.starter_location
   body = {
     properties = {}
@@ -60,7 +55,7 @@ resource "azapi_resource" "resource_group_management" {
 resource "azapi_resource" "resource_group_connectivity" {
   parent_id = "/subscriptions/${var.subscription_ids["connectivity"]}"
   type      = "Microsoft.Resources/resourceGroups@2021-04-01"
-  name      = "e2e-test-connectivity-azapi-${random_string.example.result}"
+  name      = "e2e-test-connectivity-azapi-${var.resource_name_suffix}"
   location  = local.starter_location
   body = {
     properties = {}
@@ -71,7 +66,7 @@ resource "azapi_resource" "resource_group_connectivity" {
 resource "azapi_resource" "resource_group_identity" {
   parent_id = "/subscriptions/${var.subscription_ids["identity"]}"
   type      = "Microsoft.Resources/resourceGroups@2021-04-01"
-  name      = "e2e-test-identity-azapi-${random_string.example.result}"
+  name      = "e2e-test-identity-azapi-${var.resource_name_suffix}"
   location  = local.starter_location
   body = {
     properties = {}
@@ -82,7 +77,7 @@ resource "azapi_resource" "resource_group_identity" {
 resource "azapi_resource" "resource_group_security" {
   parent_id = "/subscriptions/${var.subscription_ids["security"]}"
   type      = "Microsoft.Resources/resourceGroups@2021-04-01"
-  name      = "e2e-test-security-azapi-${random_string.example.result}"
+  name      = "e2e-test-security-azapi-${rvar.resource_name_suffix}"
   location  = local.starter_location
   body = {
     properties = {}
--- templates/test_nested/modules/test_module/main.tf
+++
@@ -1,3 +0,0 @@
-data "azurerm_subscription" "current" {
-  subscription_id = var.subscription_id
-}
