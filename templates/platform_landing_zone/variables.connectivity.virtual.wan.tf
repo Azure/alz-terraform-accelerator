b58a9af6999f854eaa1d9ecbfe52abfe0a370fc6
Prince Nagar (BEYONDSOFT CONSULTING INC) <v-prinnagar@microsoft.com>
2026-09-16 17:23:54 +0530

    feat: expose firewall_policy.location override for Virtual WAN template

--- templates/platform_landing_zone/variables.connectivity.virtual.wan.tf
+++ templates/platform_landing_zone/variables.connectivity.virtual.wan.tf
@@ -272,6 +272,7 @@ variable "virtual_hubs" {
     firewall_policy = optional(object({
       name                              = optional(string)
       resource_group_name               = optional(string)
+      location                          = optional(string)
       sku                               = optional(string, "Standard")
       auto_learn_private_ranges_enabled = optional(bool)
       base_policy_id                    = optional(string)
@@ -771,6 +772,7 @@ The following top level attributes are supported:
 - `firewall_policy` - (Optional) An object with the following fields:
   - `name` - (Optional) The name of the firewall policy. If not specified will use `afw-policy-{vnetname}`.
   - `resource_group_name` - (Optional) The name of the resource group where the firewall policy should be created. If not specified will use the parent resource group of the virtual network.
+  - `location` - (Optional) The Azure region for the firewall policy. Defaults to the hub's location when omitted, `null`, or an empty string. Use this to place the policy in the same region as a shared `base_policy_id`, since a parent and child firewall policy must reside in the same region even though the associated firewall can remain in its own hub region. Changing the location of an existing firewall policy replaces it.
   - `sku` - (Optional) The SKU to use for the firewall policy. Possible values include `Standard`, `Premium`. Default `Standard`.
   - `auto_learn_private_ranges_enabled` - (Optional) Should the firewall policy automatically learn private ranges? Default `false`.
   - `base_policy_id` - (Optional) The resource id of the base policy to use for the firewall policy.
